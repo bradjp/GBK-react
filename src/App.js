6246b7977ede6d203e52d5b95fa3769d95e05219
Brad Phillips <bjrp@protonmail.com>
2020-04-25 13:58:51 +0100

    Change to Component for App; add layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import {Layout, Navigation, Content, Drawer, Header} from 'react-mdl';
 
-function App() {
-  return (
-    <div>
-      <h1>Test</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="demo-big-content">
+          <Layout>
+              <Header title="Title" scroll>
+                  <Navigation>
+                      <a href='/'>Link</a>
+                      <a href='/'>Link</a>
+                      <a href='/'>Link</a>
+                      <a href='/'>Link</a>
+                  </Navigation>
+              </Header>
+              <Drawer title="Title">
+                  <Navigation>
+                      <a href='/'>Link</a>
+                      <a href='/'>Link</a>
+                      <a href='/'>Link</a>
+                      <a href='/'>Link</a>
+                  </Navigation>
+              </Drawer>
+              <Content>
+                  <div className="page-content" />
+              </Content>
+          </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
