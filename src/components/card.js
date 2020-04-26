4267a00eae262196c108c50ad9b37102dd9c042d
Brad Phillips <bjrp@protonmail.com>
2020-04-26 16:06:25 +0100

    Adds card component

---
+++ src/components/card.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+class Card extends Component{
+  render() {
+    return(
+      <div>
+        <h1>Testing Card</h1>
+      </div>
+    )
+  }
+}
+
+export default Card;
